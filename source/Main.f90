64827d7d498e84452fad4880e0f54c44dfc684bb
Tina N Mihm <tmihm@argon-login-1.hpc>
2021-01-26 20:31:47 -0600

    updating Main.f90 with input derived types

--- source/Main.f90
+++ source/Main.f90
@@ -6,13 +6,13 @@
       Use CCD
       Implicit None
 ! Dimensioning variables
-      Integer :: NOcc, NAO
-      Logical :: DoRing, DoXRing, DoLadder, DoMosaic
-      Integer :: IRangeRing, IRangeXRing, IRangeLadder, IRangeMosaic
-      Integer :: IRangeDriverDirect, IRangeDriverExchange, IRangeEnergy
-      Integer :: IRangeLinRings, IRangeQuadRings, IRangeDirectRings, IRangeExchangeRings  
-      Integer :: IRangeLinLadders, IRangeQuadLadders, IRangeDirectLadders, IRangeExchangeLadders
-      Real (Kind=pr) :: Rs
+!!      Integer :: NOcc, NAO
+!!      Logical :: DoRing, DoXRing, DoLadder, DoMosaic
+!!      Integer :: IRangeRing, IRangeXRing, IRangeLadder, IRangeMosaic
+!!      Integer :: IRangeDriverDirect, IRangeDriverExchange, IRangeEnergy
+!!      Integer :: IRangeLinRings, IRangeQuadRings, IRangeDirectRings, IRangeExchangeRings  
+!!      Integer :: IRangeLinLadders, IRangeQuadLadders, IRangeDirectLadders, IRangeExchangeLadders
+!!      Real (Kind=pr) :: Rs
 ! Correlated stuff
       Real (Kind=pr) :: ECorr
       Real (Kind=pr), Allocatable :: T2aaaa(:,:,:), T2abab(:,:,:), T2abba(:,:,:)
@@ -21,7 +21,7 @@
       Integer, Parameter :: NAlloc = 6
       Integer :: IAlloc(NAlloc)
       Logical, Parameter :: T = .true., F=.false.
-
+ 
 !==========================================!
 !  This code implements RHF-based CCD.     !
 !  We give it the option of keeping the    !
@@ -31,39 +31,36 @@
 !  The first thing we must do is read the  !
 !  basic information for the calculation.  !
 !==========================================!
-
-      Call ReadInput(UEG_Inp)
-
+      type UEGInput
+          include "UEG_Input.f90"!!type(UEG_Input), Intent(Out) :: UEGInput
+      end type UEGInput
+      
+      Call ReadInput(UEGInput)
 
 !==========================================!
 !  Now we can allocate the memory and go!  !
 !==========================================!
 
       IAlloc = 0
-      Allocate(T2aaaa(NOcc,NOcc,NOcc+1:NAO),  Stat=IAlloc(1))
-      Allocate(T2abab(NOcc,NOcc,NOcc+1:NAO),  Stat=IAlloc(2))
-      Allocate(T2abba(NOcc,NOcc,NOcc+1:NAO),  Stat=IAlloc(3))
-      Allocate(X2aaaa(NOcc,NOcc,NOcc+1:NAO),  Stat=IAlloc(4))
-      Allocate(X2abab(NOcc,NOcc,NOcc+1:NAO),  Stat=IAlloc(5))
-      Allocate(X2abba(NOcc,NOcc,NOcc+1:NAO),  Stat=IAlloc(6))
+      Allocate(T2aaaa(UEG_Inp%NOcc,UEG_Inp%NOcc,UEG_Inp%NOcc+1:UEG_Inp%NAO),  Stat=IAlloc(1))
+      Allocate(T2abab(UEG_Inp%NOcc,UEG_Inp%NOcc,UEG_Inp%NOcc+1:UEG_Inp%NAO),  Stat=IAlloc(2))
+      Allocate(T2abba(UEG_Inp%NOcc,UEG_Inp%NOcc,UEG_Inp%NOcc+1:UEG_Inp%NAO),  Stat=IAlloc(3))
+      Allocate(X2aaaa(UEG_Inp%NOcc,UEG_Inp%NOcc,UEG_Inp%NOcc+1:UEG_Inp%NAO),  Stat=IAlloc(4))
+      Allocate(X2abab(UEG_Inp%NOcc,UEG_Inp%NOcc,UEG_Inp%NOcc+1:UEG_Inp%NAO),  Stat=IAlloc(5))
+      Allocate(X2abba(UEG_Inp%NOcc,UEG_Inp%NOcc,UEG_Inp%NOcc+1:UEG_Inp%NAO),  Stat=IAlloc(6))
       If(Any(IAlloc /= 0)) Stop "Could not allocate in main"
       Open(7,File='Output',Status="Replace")
       Close(7)
 
 !=====================================================================!
-      Call DrvMBPT(Eigen,X2aaaa,X2abab,X2abba,NOcc,NAO,EHF,ECorr)
+      Call DrvMBPT(Eigen,X2aaaa,X2abab,X2abba,UEG_Inp%NOcc,UEG_Inp%NAO,EHF,ECorr)
 !!      If(iRSPoint == 1) Then
       T2aaaa = X2aaaa
       T2abab = X2abab
       T2abba = X2abba
 !!       End If
         
-       Call DrvCCD(Eigen,T2aaaa,T2abab,T2abba,NOcc,NAO,EHF,ECorr, &
-                   DoRing,DoXRing,DoLadder,DoMosaic, &
-                   IRangeRing,IRangeXRing,IRangeLadder,IRangeMosaic, &
-                   IRangeDriverDirect,IRangeDriverExchange,IRangeEnergy,          &
-                   IRangeLinRings,IRangeQuadRings,IRangeDirectRings,IRangeExchangeRings,    &
-                   IRangeLinLadders,IRangeQuadLadders,IRangeDirectLadders,IRangeExchangeLadders)
+       Call DrvCCD(Eigen,T2aaaa,T2abab,T2abba,UEG_Inp)
 
 !==============================================!
 !  Lastly, deallocate memory and exit safely.  !
